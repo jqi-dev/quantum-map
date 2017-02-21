07dd967124e9e6e510d4a8acef8764cf741b3a3c
Hojin Sylvia Kang <hojinskang@gmail.com>
2017-02-21 13:14:18 -0500

    Pop-up message indicating that searched term doesn't exist

--- js/graph.js
+++ js/graph.js
@@ -468,15 +468,20 @@ d3.select("#search_node").on("click", function() {
     var searchElement = document.getElementById("search_bar").value;
     var i;
     var searchedNode;
+    // find searched term
     for (i = 0; i < nodes.length; i++) {
         if (nodes[i].name.toUpperCase() === searchElement.toUpperCase()) {
             searchedNode = nodes[i];
-            console.log(searchedNode.name);
         }
     }
-    append_text(searchedNode);
-    var target = d3.selectAll("circle").filter(function(d) {
-        return d.id == searchedNode.id;
-    });
-    toggle_class(target);
+    // ensure searched term exists
+    if (searchedNode) {
+        append_text(searchedNode);
+        var target = d3.selectAll("circle").filter(function(d) {
+            return d.id == searchedNode.id;
+        });
+        toggle_class(target);
+    } else {
+      window.alert("This term does not exist!");
+    }
 });
